63e954c804b5f1589824c253917a4985e4d37f8c
rain1 <rain1@airmail.cc>
2018-09-06 02:41:16 +0100

    Create main.rkt

---
+++ main.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require peg)
+;(provide (all-from-out peg))
+(module reader racket
+
+(require syntax/strip-context)
+
+(require peg/peg)
+(require peg/peg-result)
+(require peg/peg-in-peg)
+(require peg/peg-to-scheme)
+
+(provide (rename-out [literal-read read]
+                     [literal-read-syntax read-syntax]))
+
+(define (peg-port->scheme in)
+  (peg->scheme (car
+                  (peg-result->object (peg (and peg (! (any-char))) (port->string in))))))
+
+(define (literal-read in)
+  (syntax->datum
+   (literal-read-syntax #f in)))
+
+(define (literal-read-syntax src in)
+  (with-syntax ([body (peg-port->scheme in)])
+    (strip-context
+     #'(module anything racket
+         (provide (all-defined-out))
+         (require peg/peg)
+         body))))
+
+)
