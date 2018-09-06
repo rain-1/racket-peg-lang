652c2204b54d748c8b3789e723b52be36fbd00e0
rain1 <rain1@airmail.cc>
2018-09-06 02:42:50 +0100

    info

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define version "0.1")
+(define collection "peg-lang")
+(define deps '("base" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+;(define scribblings '(("scribblings/peg.scrbl" ())))
+;(define compile-omit-paths '("peg-src"))
+(define pkg-desc "A PEG parser generator")
